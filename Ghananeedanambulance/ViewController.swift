e23a66a171e260e8ee82c8eb4970faa5e42ac18a
Jonathan Lamptey <nii3601@hotmail.com>
2018-11-02 21:06:36 -0400

    Test Commit

---
+++ Ghananeedanambulance/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Ghananeedanambulance
+//
+//  Created by NiiDjan Lamptey on 02/11/2018.
+//  Copyright © 2018 Ghananeedanambulance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello World")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
