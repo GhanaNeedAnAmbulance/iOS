1025a9c4716b82d5f0dde949f0602676fafb2938
Jonathan Lamptey <nii3601@hotmail.com>
2018-11-02 22:26:20 -0400

    Commenting

--- Ghananeedanambulance/ViewController.swift
+++ Ghananeedanambulance/ViewController.swift
@@ -13,11 +13,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello World")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    //this is just a test
     override func loadView() {
+        print("Loading View")
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
@@ -31,6 +33,7 @@ class ViewController: UIViewController {
         marker.snippet = "Australia"
         marker.map = mapView
     }
-
+//
+    
 }
 
