6c15a52894adbbf1999584369cc87afda1fc9804
Jonathan Lamptey <nii3601@hotmail.com>
2018-11-03 11:22:51 -0400

    More Editing

--- Ghananeedanambulance/ViewController.swift
+++ Ghananeedanambulance/ViewController.swift
@@ -22,15 +22,15 @@ class ViewController: UIViewController {
         print("Loading View")
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: 48.68316, longitude: -86.25055, zoom: 8.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        marker.position = CLLocationCoordinate2D(latitude: 48.68316, longitude: -86.25055)
+        marker.title = "Test"
+        marker.snippet = "Test Address"
         marker.map = mapView
     }
 //
